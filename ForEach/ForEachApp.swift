02b02480b578fee8bceeca21e4199227f203e23e
Philip Keller <philipjoelkeller@gmail.com>
2023-03-14 14:37:52 -0700

    Initial Commit

---
+++ ForEach/ForEachApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForEachApp.swift
+//  ForEach
+//
+//  Created by Philip Keller on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ForEachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
