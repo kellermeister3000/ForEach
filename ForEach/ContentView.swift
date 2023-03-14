c9c88cc6e1a495d4f0aeaf3820f0e2b73722a8e1
Philip Keller <philipjoelkeller@gmail.com>
2023-03-14 14:45:41 -0700

    Over Engineered Tabbed Picker (Potter Themed)

--- ForEach/ContentView.swift
+++ ForEach/ContentView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        Form {
-            ForEach(0..<100) {
-                Text("Row \($0)")
+        NavigationStack {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
             }
         }
     }
